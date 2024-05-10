a7c709fbd647b21ddd987384d443aa6ae8cf775d
guguqiui <lvqiuyuan1101@163.com>
2024-05-10 21:40:17 +0800

    add SpringCache

--- src/main/java/org/example/service/BookService.java
+++ src/main/java/org/example/service/BookService.java
@@ -2,6 +2,9 @@ package org.example.service;
 
 import org.example.entity.Books;
 import org.example.mapper.MainMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -9,41 +12,37 @@ import java.util.List;
 
 @Service
 public class BookService {
-    //    数据源
     @Resource
     MainMapper mapper;
 
-    // 分页查询书籍列表
+    @Cacheable(value = "booksPaged", key = "#name + '_' + #page + '_' + #pageSize")
     public List<Books> getBooksPaged(String name, int page, int pageSize) {
         int offset = page * pageSize;
         return mapper.bookListPaged(name, offset, pageSize);
     }
 
-    //    显示所有书籍的列表
+    @Cacheable(value = "bookList", key = "#name")
     public List<Books> bookList(String name) {
         return mapper.bookList("%");
     }
 
-    //    删除书籍操作
+    @CacheEvict(value = {"booksPaged", "bookList"}, allEntries = true)
     public void Del(int bookID) {
         mapper.Del(bookID);
     }
 
-    //    添加书籍操作
+    @CacheEvict(value = {"booksPaged", "bookList"}, allEntries = true)
     public void addBook(String name, int count, String detail) {
         mapper.addBook(name, count, detail);
     }
 
-    //    修改书籍操作，这里由于是涉及到了修改，所以自然要回显数据
+    @Cacheable(value = "books", key = "#bookID")
     public Books findByID(int bookID) {
         return mapper.findByID(bookID);
     }
 
-    //    更新操作
+    @CacheEvict(value = {"booksPaged", "bookList", "books"}, allEntries = true)
     public void update(int id, String name, int counts, String detail) {
         mapper.update(id, name, counts, detail);
     }
-
-
 }
-
